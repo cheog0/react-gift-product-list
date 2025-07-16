69cbbd19e4bcdf3f80ff34017c3aaca01b12a329
cheog0 <https://github.com/cheog0>
2025-07-16 15:11:29 +0900

    refactor: fetch 요청 시 URL 객체 사용

--- src/hooks/useRankingProducts.ts
+++ src/hooks/useRankingProducts.ts
@@ -3,11 +3,13 @@ import type { Product } from '@/types';
 
 export function useRankingProducts(targetType: string, rankType: string) {
   const apiUrl = import.meta.env.VITE_API_URL;
-  const url = `${apiUrl}/api/products/ranking?targetType=${targetType}&rankType=${rankType}`;
+  const url = new URL('/api/products/ranking', apiUrl);
+  url.searchParams.set('targetType', targetType);
+  url.searchParams.set('rankType', rankType);
   const {
     data: products,
     loading,
     error,
-  } = useFetch<Product[]>(url, [targetType, rankType]);
+  } = useFetch<Product[]>(url.toString(), [targetType, rankType]);
   return { products: products || [], loading, error };
 }
